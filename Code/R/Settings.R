8f79670b547f1788521a2c495597b6f1d692e8c3
calahan <deancalahan@comcast.net>
2018-12-17 13:30:27 -0500

    Finishing up initial work.

--- Code/R/Settings.R
+++ Code/R/Settings.R
@@ -19,7 +19,7 @@ HOBO_fn <- paste0(csv_dir, c("ATS_1.11.09.18.csv",
                             "ATS_1.22.10.18.csv"))
 
 #fig_fn <- c("Visual Elements/Figures/Figure 1.png", "blah")
-fig_fn <- c("Figures/Figure1.png", "blah")
+fig_fn <- c("Figures/Figure1.png", "Visual Elements/Figures/Figure2.png")
 
 # Spreadsheet of water chemistry and biomass observations
 ss_fn <- paste0(ss_dir, "ATS Treatment.xlsx")
