313caa863f8b10585ee1fc6cdaae36a3a61b7e47
calahan <deancalahan@comcast.net>
2019-01-12 17:34:37 -0500

    Beginning to finalize Data Cleaning

--- Code/R/FZJWWTPP.R
+++ Code/R/FZJWWTPP.R
@@ -1,10 +1,39 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Copyright 2018-2019 by Forschungszentrum-Jülich (FZ-J)
+#
+# This file is part of the internal publication "FZ-J WWTP Pilot" (FZJWWTPP), a
+# literate program that documents data cleaning procedures for the creation of several
+# data sets arising from a project, running from July 1 through August 31 2018,
+# that demonstrated operation of an algal turf scrubber at FZ-J's Jülich campus
+# wastewater treatment plant.
+#
+# The software component of FZJWWTPP is open access: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+
+# The software component of FZJWWTPP is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with the
+# software component of FZJWWTPP If not, see <http://www.gnu.org/licenses/>.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+#source("Settings.R")
+
 # Functions to support analysis of data from FZ-J WWTP ATS Pilot
-HOBOData <- function(fn, zone="Europe/Berlin", trim=c(NA, NA), wind=c(NA, NA), view=TRUE, text=FALSE, ret=FALSE, week=NA) {
+HOBOData <- function(fn, zone="Europe/Berlin", trim=c(NA, NA), window=c(NA, NA), view=TRUE, text=FALSE, ret=FALSE, week=NA) {
+# Load and optionally trim or plot data from a HOBO data logger.
 #
 # i:
 # fn        Name of the HOBO csv file to load
+# zone      Time zone in which the HOBO readings were recorded
 # trim      Number of observations to remove from beginning and end of data
-# wind      Beginning and end indices of data to plot (if view==TRUE)
+# window    Beginning and end indices of data to plot (if view==TRUE)
 # view      Whether to plot the data
 # text      Whether to display text with count of observations in the csv file
 # ret       Whether to return the trimmed dataset
@@ -41,8 +70,8 @@ HOBOData <- function(fn, zone="Europe/Berlin", trim=c(NA, NA), wind=c(NA, NA), v
     trim <- c(trim[1], row_ct - trim[2])
     }
 
-    if(is.na(wind[1])) {
-        wind <- c(1, row_ct)
+    if(is.na(window[1])) {
+        window <- c(1, row_ct)
     }
 
     if(text) {
@@ -53,8 +82,8 @@ HOBOData <- function(fn, zone="Europe/Berlin", trim=c(NA, NA), wind=c(NA, NA), v
         par(mfrow=c(1,2))
         par(ps = 12, cex = 1, cex.main = 1)
 
-        plot(x=tb[wind[1]:wind[2],]$datetime,
-             y=tb[wind[1]:wind[2],]$lux,
+        plot(x=tb[window[1]:window[2],]$datetime,
+             y=tb[window[1]:window[2],]$lux,
              type="l",
              main=paste0("lux ", fn),
              xlab="Date",
@@ -66,8 +95,8 @@ HOBOData <- function(fn, zone="Europe/Berlin", trim=c(NA, NA), wind=c(NA, NA), v
                lty=c(1, 1),
                lwd=c(1, 1))
 
-        plot(x=tb[wind[1]:wind[2],]$datetime,
-             y=tb[wind[1]:wind[2],]$temp,
+        plot(x=tb[window[1]:window[2],]$datetime,
+             y=tb[window[1]:window[2],]$temp,
              type="l",
              main=paste0("temp ", fn),
              xlab="Date",
@@ -109,8 +138,8 @@ CleanHOBOData <- function(fn, l, r, w) {
     tb <- HOBOData(fn, view=FALSE, ret=TRUE, week=w)
     row_ct <- nrow(tb)
     HOBOData(fn, trim=c(l, r))
-    HOBOData(fn, trim=c(l, r), wind=c(l-50, l+50))
-    HOBOData(fn, trim=c(l, r), wind=c(row_ct-r-50, row_ct-r+50))
+    HOBOData(fn, trim=c(l, r), window=c(l-50, l+50))
+    HOBOData(fn, trim=c(l, r), window=c(row_ct-r-50, row_ct-r+50))
     return(tb[l:(row_ct-r),])
 }
 
@@ -152,6 +181,82 @@ IntegrateObs <- function(d, df1, df2, diag=FALSE) {
     }
 
     return(c(sum(temps), sum(luxs)))
+}
+
+# Create a cleaned data frame from the spreadsheet "ATS Treatment"
+WaterQualityBiomass <- function() {
+# i: NA
+# v: A cleaned data frame containing results from water chemistry analsysis
+# s: Creation of the csv file "FZJ WWTP ATS Pilot Chemistry and Biomass.csv"
+
+    # Read the spreadsheet
+    t_df <- read_excel(ss_fn,
+                       sheet="Tabelle1",
+                       skip=3,
+                       col_names=c("date",
+                                   "time",
+                                   "beforeafter",
+                                   "PO4dil",
+                                   "PO4P",
+                                   "PO4Pxdil",
+                                   "PO4Pred",
+                                   "dilTN",
+                                   "TNxdil",
+                                   "TN",
+                                   "TNred",
+                                   "comment",
+                                   "remark",
+                                   "assaydate",
+                                   "bmwet",
+                                   "bmdry",
+                                   "bmdrypct",
+                                   "pH"))
+
+    # Impute missing times. We take the earliest time, and simply add 24 hours to it
+    # [todo] Is this even used? There is something wrong here at any rate
+    no_times <- which(is.na(t_df$time))
+    min_time <- min(ti_df[ti_df$week == 1,]$datetime)
+    t_df[which(is.na(t_df$time)),]$time <- make_datetime(2018,
+                                                         12,
+                                                         31,
+                                                         8,
+                                                         0,
+                                                         0)
+
+    # Create a new data frame with date and time combined into datetime for each observation
+    wqb_df <- data.frame(datetime=make_datetime(year(t_df$date),
+                                                month(t_df$date),
+                                                day(t_df$date),
+                                                hour(t_df$time),
+                                                minute(t_df$time),
+                                                second(t_df$time),
+                                                tz="Europe/Berlin"))
+
+    # Make before/after a logical
+    wqb_df$before <- TRUE
+    wqb_df[which(t_df$beforeafter != "before"),]$before <- FALSE
+
+    # Nutrient concentrations & pH
+    wqb_df$PO4P <- t_df$PO4Pxdil
+    wqb_df$TN <- t_df$TN
+    wqb_df$pH <- t_df$pH
+
+    # Make frozen/not frozen a logical
+    wqb_df$frozen <- TRUE
+    wqb_df[which(t_df$comment != "frozen"),]$frozen <- FALSE
+
+    # Date of assay
+    wqb_df$assaydate <- make_date(year(t_df$assaydate),
+                                  month(t_df$assaydate),
+                                  day(t_df$assaydate))
+
+    # Biomass measurements
+    wqb_df$wet_biomass <- t_df$bmwet
+    wqb_df$dry_biomass <- t_df$bmdry
+    wqb_df$solids <- t_df$bmdrypct
+
+    # Need observation date separate from datetime to make life easier
+    wqb_df$obsdate <- make_date(year(wqb_df$datetime), month(wqb_df$datetime), day(wqb_df$datetime))
 }
 
 BiomassData <- function() {
--- Code/R/Settings.R
+++ Code/R/Settings.R
@@ -1,3 +1,13 @@
+# Libraries
+library(readr)
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(knitr)
+library(PeriodicTable)
+library(ggplot2)
+
 # Directories
 data_dir <- "Data/"
 csv_dir <- paste0(data_dir, "csv/")
@@ -8,7 +18,7 @@ yeardays <- 365     # days per year
 yearweeks <- 52     # weeks per year
 C2K <- 273.15       # convert °C to °K
 
-# Input filenames
+# INPUT FILENAMES
 # csv files exported from HOBO downloads
 HOBO_fn <- paste0(csv_dir, c("ATS_1.11.09.18.csv",
                             "ATS_2.17.09.18.csv",
@@ -19,20 +29,26 @@ HOBO_fn <- paste0(csv_dir, c("ATS_1.11.09.18.csv",
                             "ATS_1.22.10.18.csv"))
 
 #fig_fn <- c("Visual Elements/Figures/Figure 1.png", "blah")
-fig_fn <- c("Figures/Figure1.png", "Visual Elements/Figures/Figure2.png")
+#fig_fn <- c("Figures/Figure1.png", "Visual Elements/Figures/Figure2.png")
 
 # Spreadsheet of water chemistry and biomass observations
-ss_fn <- paste0(ss_dir, "ATS Treatment.xlsx")
+sswc_fn <- paste0(ss_dir, "ATS Treatment.xlsx")
 
 # Spreadsheet of biomass composition data
-bcss_fn <- paste0(ss_dir, "Analysen ZEA-3.xlsx")
+ssbc_fn <- paste0(ss_dir, "Analysen ZEA-3.xlsx")
 
-# Output filenames
+# OUTPUT FILENAMES
+pre <- "FZJ WWTP ATS Pilot "
+ext <- ".csv"
+
 # Cleaned temperature and illumination observations
-ti_fn <- paste0(csv_dir, "FZJ WWTP ATS Pilot Temperature and Illumination.csv")
+ti_fn <- paste0(csv_dir, pre, "Temperature and Illumination", ext)
 
 # Cleaned water chemistry and biomass observations
-wqb_fn <- paste0(csv_dir, "FZJ WWTP ATS Pilot Chemistry and Biomass.csv")
+wcb_fn <- paste0(csv_dir, pre, "Chemistry and Biomass", ext)
 
 # Cleaned biomass composition
-bc_fn <- paste0(csv_dir, "FZJ WWTP ATS Pilot Biomass Composition.csv")
+bc_fn <- paste0(csv_dir, pre, "Pilot Biomass Composition", ext)
+
+# Biomass means and ratios
+bmr_fn <- paste0(csv_dir, pre, "Biomass Means and Ratios", ext)
