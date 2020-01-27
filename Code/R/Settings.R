5955e18386c786e8cc430d344e86387e685bfa42
Calahan <s.calahan@fz-juelich.de>
2020-01-27 10:10:22 +0100

    Getting ready to start building the data set.

--- Code/R/Settings.R
+++ Code/R/Settings.R
@@ -1,29 +1,3 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#
-# Copyright 2018-2019 by Forschungszentrum-Jülich (FZ-J)
-#
-# This file is part of the publication "FZJ ATS Pilot 2018" (FZJAP2018), a data
-# set documenting the results of an algal turf scrubbing pilot project performed
-# 15.07-15.09 2018, treating secondary sewage at FZ-J's campus wastewater treatment
-# plant.
-#
-# The software component of FZJAP2018 is open access: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your option)
-# any later version.
-#
-# The software component of FZJAP2018 is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with the
-# software component of FZJAP2018. If not, see <http://www.gnu.org/licenses/>.
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# libraries
-library(readr)
 library(readxl)
 library(dplyr)
 library(tidyr)
@@ -32,10 +6,20 @@ library(knitr)
 library(PeriodicTable)
 library(ggplot2)
 
-# directories
-data_dir <- "Data/"
+# input data directories
+src_data_dir <- "K:\\Projekte\\ATS\\Data\\"
+src_fw_dir <- list();
+src_fw_dir[c("Novagreen", "WWTP Pilot")] <-
+    c(paste0(src_data_dir, "Novagreen\\"),
+      paste0(src_data_dir, "WWTP Pilot\\"))
+
+# input data file names
+
+
+data_dir <- "../../Data/"
 csv_dir <- paste0(data_dir, "csv/")
-ss_dir <- paste0(data_dir, "xlsx/")
+xlsx_dir <- paste0(data_dir, "xlsx/")
+
 
 # conversion factors
 #yr2d <- 365     # days per year
@@ -53,8 +37,8 @@ HOBO_fn <- paste0(csv_dir, c("ATS_1.11.09.18.csv",
                              "ATS_1.22.10.18.csv"))
 
 # i: spreadsheets
-sswc_fn <- paste0(ss_dir, "ATS Treatment.xlsx")  # water chemistry and biomass
-ssbc_fn <- paste0(ss_dir, "Analysen ZEA-3.xlsx") # of biomass composition
+sswc_fn <- paste0(xlsx_dir, "ATS Treatment.xlsx")  # water chemistry and biomass
+ssbc_fn <- paste0(xlsx_dir, "Analysen ZEA-3.xlsx") # of biomass composition
 
 # o: csv files
 pre <- "FZJAP2018 "
